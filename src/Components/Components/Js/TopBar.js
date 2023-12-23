4f6c2f1e2bc930d3df651cc1d12ac806c1a4f5e4
ayoub bedraoui <ayoubbedraoui18@gmail.com>
2023-12-23 15:46:43 +0100

    changes topbar

--- src/Components/Components/Js/TopBar.js
+++ src/Components/Components/Js/TopBar.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React,{useState} from 'react';
 import '../Css/TopBar.css';
 import ProfileIcon from '../../Global/Top-Bar/js/ProfileIcon';
 import ProfileMenu from '../../Global/Top-Bar/js/ProfileMenu';
 
 function TopBar() {
+  const [openProfile,setOpenProfile] = useState(false);
   return (
+    <>
+
     <div className='TopBar__container'>
       <div className='TopBar__cadre'>
         <div>
         <button className='search__button'>Search</button>
         </div>
         <div>
-        <ProfileIcon 
+          <div onClick={() => {setOpenProfile(!openProfile)}}>
+          <ProfileIcon  
+          
           ProfileIconImg='../images/avatar/1.png'
         />
-        <ProfileMenu />
+          </div>
+        <div className={`dropdown__container ${openProfile? 'active' : 'inactive'} `}>
+        <ProfileMenu   />
+
+        </div>
         </div>
         
       </div>
     </div>
+    {
+      openProfile && <ProfileMenu />
+    }  
+    </>
   )
 }
 
--- src/Pages/View/Home/Js/Espace_Etudiant.js
+++ src/Pages/View/Home/Js/Espace_Etudiant.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../Css/Espace_Etudiant.css';
 import Menu from '../../../../Components/Mini-Components/Js/Menu'
 import TopBar from '../../../../Components/Components/Js/TopBar'
 
@@ -6,11 +7,13 @@ function Espace_Etudiant() {
   return (
     <div>
       <div className='espace__etudiant__container'>
-        <div>
-        <Menu/>
+        <div className='menu'>
+          <Menu/>
         </div>
-        <div>
-        <TopBar />
+        <div className='content'>
+          <div>
+          <TopBar />
+          </div>
         </div>
       </div>
     </div>
