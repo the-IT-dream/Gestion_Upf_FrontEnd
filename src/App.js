9d41e8e4a4a2ced75cba19cac4c0c677dc0e5521
ayoub bedraoui <ayoubbedraoui18@gmail.com>
2023-12-23 21:51:28 +0100

    Espace etudiant

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import './App.css';
 
-import BackgroundLogin from './Components/Components/Js/BackgroundLogin';
-import { Button } from './Components/Mini-Components/Js/Button';
-import BackgroundLogin from './Components/Components/Js/BackgroundLogin';
-import ForgotPassword from './Pages/Auth/Js/ForgotPassword';
 import Login from './Pages/Auth/Js/Login';
-import ResetPassword from './Pages/Auth/Js/ResetPassword';
 
 
 function App() {
 
   return (
 
-   <div className='app_css' style={{ backgroundImage: `url('${backgroundImagePath}')` }}>
+   <div className='app_css' >
     <div>
       <Login/>
     </div>
