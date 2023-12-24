24dd596a284e244edc5758379fd248c8f4ddb852
amine <mohamedaminemliha10@gmail.com>
2023-12-24 19:50:38 +0100

    sidebar changes

--- src/index.js
+++ src/index.js
@@ -8,20 +8,25 @@ import ResetPassword from './Pages/Auth/Js/ResetPassword';
 import ForgotPassword from './Pages/Auth/Js/ForgotPassword';
 import Login from './Pages/Auth/Js/Login';
 import SideBar from './Components/Mini-Components/Js/Navbar';
+import Dashboard from '../src/Components/Mini-Components/SideBar/pages/Dashboard'
 
 
 
 
 ReactDOM.render(
     // <App />,
-    <Router>
-        <Routes>
-            <Route path='/' element={<App />} />      
-            <Route path='/Login' element={<Login />} />
-            <Route path='/ForgotPassword' element={<ForgotPassword />} />
-            <Route path='/ResetPassword' element={<ResetPassword />} />
-            <Route path='/Espace_Etudiant' element={<Espace_Etudiant />} />
-        </Routes>
-    </Router>,
+    // <Router>
+    //     <Routes>
+    //         <Route path='/' element={<Dashboard />} />      
+    //         <Route path='/Login' element={<Login />} />
+    //         <Route path='/ForgotPassword' element={<ForgotPassword />} />
+    //         <Route path='/ResetPassword' element={<ResetPassword />} />
+    //         <Route path='/Espace_Etudiant' element={<Espace_Etudiant />} />
+    //         <Route path="/Espace_Etudiant/Dashboard" element={<Dashboard />}/>
+    //     </Routes>
+    // </Router>,
+    <React.StrictMode>
+    <Espace_Etudiant />
+     </React.StrictMode>,
     document.getElementById('root') 
 );
